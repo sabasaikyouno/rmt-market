6b5d743d2250b8027d84ef51a04670002d4ffe69
sabasaikyouno <0852kuro@gmail.com>
2024-04-02 18:03:22 +0900

    GMarketDT Json

--- app/models/GMarketDT.scala
+++ app/models/GMarketDT.scala
@@ -1,5 +1,7 @@
 package models
 
+import play.api.libs.json.{Json, OWrites, Reads}
+
 case class GMarketDT(
   title: String,
   imgSrc: String,
@@ -10,3 +12,8 @@ case class GMarketDT(
   category: String,
   site: String
 )
+
+object GMarketDT {
+  implicit val jsonWrites: OWrites[GMarketDT] = Json.writes[GMarketDT]
+  implicit val jsonReads: Reads[GMarketDT] = Json.reads[GMarketDT]
+}
