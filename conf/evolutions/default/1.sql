730f798f0705ecded858dc7ac3fac97dcba39f6f
sabasaikyouno <0852kuro@gmail.com>
2024-04-08 15:33:56 +0900

    category, site, game titleのsql

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -18,18 +18,29 @@ create table game_data(
 
 create table category(
     category_id int(4) not null unique,
-    category varchar(100) not null unique
+    category varchar(100) not null
 ) engine=innodb charset=utf8mb4;
+
+INSERT INTO category (category_id, category)
+VALUES (1, "アカウント"), (2, "アイテム"), (3, "代行"), (4, "その他");
 
 create table site(
     site_id int(4) not null unique,
-    site varchar(100) not null unique
+    site varchar(100) not null
 ) engine=innodb charset=utf8mb4;
+
+INSERT INTO site (site_id, site)
+VALUES (1, "RMTClub"), (2, "GameClub"), (3, "GameTrade");
 
 create table game_title(
     game_title_id int(4) not null unique,
-    game_title varchar(100) not null unique
+    game_title varchar(100) not null
 ) engine=innodb charset=utf8mb4;
+
+INSERT INTO game_title (game_title_id, game_title)
+VALUES (1, "Apex"), (2, "Fortnite"), (3, "valorant"), (4, "あつ森"), (5, "ドラクエ10"), (6, "ポケモン剣盾"),
+(7, "ポケモンSV"), (8, "FF14"), (9, "モンスト"), (10, "プロスピA"), (11, "FGO"), (12, "バウンティラッシュ"),
+(13, "原神"), (14, "ポケモンGO"), (15, "荒野行動"), (16, "パズドラ");
 
 # --- !Downs
 
