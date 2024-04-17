a72a1bc6937e8c94725e2fb9f4f8d5fadffd85b2
sabasaikyouno <0852kuro@gmail.com>
2024-04-17 17:11:37 +0900

    category別でgameDataを取得できるようにした。

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -21,8 +21,8 @@ class HomeController @Inject()(
     }
   }
 
-  def getGameDataListByTitle(gameTitle: String, page:Int) = Action.async { implicit request =>
-    gameDataRepository.getByTitle(gameTitle, page).map { list =>
+  def getGameDataListByTitle(gameTitle: String, page:Int, category: String) = Action.async { implicit request =>
+    gameDataRepository.getByTitle(gameTitle, page, category).map { list =>
       Ok(Json.toJson(list))
     }
   }
